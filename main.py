ba541b7d2c92b0523a6428b7a54948a7db5d047f
Oghenovo Usiwoma <eunovo9@gmail.com>
2020-09-10 09:39:22 +0100

    Build dataset from environment and number of episodes

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+import tensorflow as tf
 import gym
 
-env = gym.make('Taxi-v3')
-env.reset()
-for _ in range(1000):
-    env.render()
-    env.step(env.action_space.sample()) # take a random action
-env.close()
+
+def get_states(environment, number_of_episodes):
+    print(environment, number_of_episodes)
+    env = gym.make(environment)
+    n = 0
+    while(n < number_of_episodes):
+        observation = env.reset()
+        done = False
+        while(not done):
+            yield observation
+            observation, reward, done, info = env.step(
+                env.action_space.sample())
+        n += 1
+
+
+dataset = tf.data.Dataset.from_generator(
+    lambda: get_states('Taxi-v3', 1), output_types=tf.int32, output_shapes=(), )
+
+# for count_batch in dataset.repeat().batch(10).take(10):
+#     print(count_batch.numpy())
