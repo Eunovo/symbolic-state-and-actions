e055bf7b7fd4515b690bb58c560346d135440a87
Oghenovo Usiwoma <eunovo9@gmail.com>
2020-10-10 16:47:44 +0100

    WIP: Implement OptionNetworks with Gumbel Distribution

---
+++ models/hierarchy/OptionsNetwork.py
@@ -0,0 +1,81 @@
+import tensorflow as tf
+
+from tf_agents.networks import network
+from tf_agents.distributions import gumbel_softmax
+from tf_agents.networks import encoding_network, utils
+from tf_agents.specs import distribution_spec, tensor_spec
+from tf_agents.utils import common as common_utils
+from tf_agents.utils import nest_utils
+
+
+class OptionsNetwork(network.DistributionNetwork):
+    def __init__(
+        self,
+        observation_spec,
+        action_spec,
+        n_options,
+        name='OptionsNetwork'
+    ):
+        self.n_options = n_options
+        self.action_spec = action_spec
+
+        output_shape = action_spec.shape  # .concatenate([2])
+        output_spec = self._output_distribution_spec(
+            output_shape, action_spec, name
+        )
+
+        super(OptionsNetwork, self).__init__(
+            input_tensor_spec=observation_spec,
+            output_spec=output_spec,
+            state_spec=(), name=name
+        )
+
+        self._output_shape = output_shape
+
+        self.inputs = tf.keras.layers.Dense(
+            observation_spec.shape.num_elements(),
+            activation='relu'
+        )
+        self.hidden_1 = tf.keras.layers.Dense(
+            1000, activation='relu'
+        )
+        self.projection_layer = tf.keras.layers.Dense(
+            self._output_shape.num_elements()
+        )
+
+    def _output_distribution_spec(self, output_shape, sample_spec, network_name):
+        input_param_spec = {
+            'temperature': tensor_spec.TensorSpec(
+                (), name=network_name + '_temp'),
+            'logits':
+                tensor_spec.TensorSpec(
+                    shape=output_shape,
+                    dtype=tf.float32,
+                    name=network_name + '_logits'
+                )
+        }
+
+        return distribution_spec.DistributionSpec(
+            gumbel_softmax.GumbelSoftmax,
+            input_param_spec,
+            sample_spec=sample_spec,
+            dtype=sample_spec.dtype
+        )
+
+    def call(
+        self, observations,
+        step_type=(), network_state=(),
+        training=False, mask=None
+    ):
+        outer_rank = nest_utils.get_outer_rank(
+            observations, self.input_tensor_spec)
+        batch_squash = utils.BatchSquash(outer_rank)
+        observations = tf.nest.map_structure(
+            batch_squash.flatten, observations)
+
+        state = self.inputs(observations)
+        state = self.hidden_1(state)
+        logits = self.projection_layer(state)
+        logits = batch_squash.unflatten(logits)
+
+        return self.output_spec.build_distribution(temperature=0.7, logits=logits), ()
--- models/hierarchy/__init__.py
+++ models/hierarchy/__init__.py
@@ -1,1 +1,2 @@
 from models.hierarchy.action_network import *
+from models.hierarchy.OptionsNetwork import *
