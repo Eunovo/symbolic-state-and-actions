d057d6f025a6b5e5431bc3e0c381bd01e154a67c
Oghenovo Usiwoma <eunovo9@gmail.com>
2020-10-10 20:29:54 +0100

    Fix input dimensions

--- models/hierarchy/action_network.py
+++ models/hierarchy/action_network.py
@@ -7,7 +7,7 @@ from layers import GumbelSoftmaxLayer
 class ActionNetwork(K.Model):
     def __init__(self, num_actions, num_state_bits, n_epochs):
         super(ActionNetwork, self).__init__()
-        self.inputs = K.layers.Dense((num_actions + num_state_bits), activation='relu')
+        self.inputs = K.layers.Dense(num_actions + num_state_bits, activation='relu')
         self.hidden_1 = K.layers.Dense(1000, activation='relu')
         self.hidden_2 = K.layers.Dense(num_state_bits * 2)
         self.gumbel = GumbelSoftmaxLayer(num_state_bits, 2, 0.7, 5.0, n_epochs)
