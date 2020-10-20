5f07526ed7fbb3525b16d577d9ed43c0d63fe0c5
Oghenovo Usiwoma <eunovo9@gmail.com>
2020-10-20 08:12:00 +0100

    Only run on main

--- train_hierarchy.py
+++ train_hierarchy.py
@@ -106,177 +106,182 @@ def populate_buffer(env, replay_buffer, policy, spec, num_steps, batch_size):
     driver.run()
 
 
-normalizer = Normalizer(0, 499)
-sae = StateAutoEncoder(
-    1, 1,
-    num_state_bits, normalize=True,
-    normalizer=normalizer
-)
-sae.use_checkpoints(encoder_path)
-
-train_env, _ = load_env(env_name, sae)
-
-master_action_spec = array_spec.BoundedArraySpec(
-    shape=((num_options,)), dtype=np.float32,
-    minimum=0, maximum=1, name='master_action'
-)
-
-options_observation_spec = array_spec.BoundedArraySpec(
-    shape=((num_options + num_state_bits), ), dtype=np.float32,
-    minimum=0, maximum=1, name='option_observation'
-)
-options_action_spec = array_spec.BoundedArraySpec(
-    shape=(num_state_bits, 2), dtype=np.float32,
-    minimum=0, maximum=1, name='option_action'
-)
-
-options_time_step_spec = ts.TimeStep(
-    step_type=train_env.time_step_spec().step_type,
-    reward=train_env.time_step_spec().reward,
-    discount=train_env.time_step_spec().discount,
-    observation=options_observation_spec
-)
-
-num_actions = train_env.action_spec().maximum - train_env.action_spec().minimum + 1
-low_level_model, callbacks = setup_model(
-    num_actions, num_state_bits, sae, low_level_model_path)
-
-low_level_env = LowLevelEnv(train_env, low_level_model)
-
-options_env = OptionsEnv(
-    low_level_env, options_observation_spec, options_action_spec)
-option_train_env = tf_py_environment.TFPyEnvironment(options_env)
-
-master_env = MasterEnv(
-    low_level_env, master_action_spec)
-master_train_env = tf_py_environment.TFPyEnvironment(master_env)
-
-optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
-global_step = tf.compat.v1.train.get_or_create_global_step()
-
-master_value_network = value_network.ValueNetwork(
-    master_train_env.time_step_spec().observation,
-    fc_layer_params=(100,)
-)
-
-master_actor_network = actor_distribution_network.ActorDistributionNetwork(
-    master_train_env.time_step_spec().observation,
-    master_train_env.action_spec(),
-    fc_layer_params=(100,)
-)
-
-master_agent = ppo_agent.PPOAgent(
-    master_train_env.time_step_spec(),
-    master_train_env.action_spec(),
-    optimizer=optimizer,
-    actor_net=master_actor_network,
-    value_net=master_value_network,
-    train_step_counter=tf.Variable(0)
-)
-master_agent.initialize()
-master_agent.train = common.function(master_agent.train)
-options_env.set_master_policy(master_agent.policy)
-
-options_actor_network = OptionsNetwork(
-    option_train_env.time_step_spec().observation,
-    option_train_env.action_spec(),
-    num_options
-)
-
-options_value_network = value_network.ValueNetwork(
-    option_train_env.time_step_spec().observation,
-    fc_layer_params=(100,)
-)
-
-options_agent = ppo_agent.PPOAgent(
-    option_train_env.time_step_spec(),
-    option_train_env.action_spec(),
-    optimizer=optimizer,
-    actor_net=options_actor_network,
-    value_net=options_value_network,
-    train_step_counter=tf.Variable(0)
-)
-options_agent.initialize()
-options_agent.train = common.function(options_agent.train)
-master_env.set_options_policy(options_agent.policy)
-
-
-master_rb = create_replay_buffer(
-    master_agent, batch_size, replay_buffer_max_length)
-options_rb = create_replay_buffer(
-    options_agent, batch_size, replay_buffer_max_length)
-
-master_ds = master_rb.as_dataset(
-    num_parallel_calls=3,
-    sample_batch_size=batch_size,
-    num_steps=2
-)
-master_iter = iter(master_ds)
-options_ds = options_rb.as_dataset(
-    num_parallel_calls=3,
-    sample_batch_size=batch_size,
-    num_steps=2
-)
-options_iter = iter(options_ds)
-
-master_checkpointer = create_train_checkpointer(
-    checkpoint_dir + "master/", master_agent, master_rb, global_step)
-options_checkpointer = create_train_checkpointer(
-    checkpoint_dir + "options/", options_agent, options_rb, global_step)
-
-master_saver = policy_saver.PolicySaver(master_agent.policy)
-options_saver = policy_saver.PolicySaver(options_agent.policy)
-
-
-def check_interval(interval):
-    return global_step % interval == 0
-
-
-while (global_step < num_iterations):
-    populate_buffer(
-        master_train_env, master_rb,
-        master_agent.collect_policy,
-        master_agent.time_step_spec,
-        master_collect_steps,
-        batch_size
+def train():
+    normalizer = Normalizer(0, 499)
+    sae = StateAutoEncoder(
+        1, 1,
+        num_state_bits, normalize=True,
+        normalizer=normalizer
     )
-    for _ in range(warmup_period):
-        experience, unused_info = next(master_iter)
-        master_loss = master_agent.train(experience)
+    sae.use_checkpoints(encoder_path)
 
-    for _ in range(joint_update_period):
+    train_env, _ = load_env(env_name, sae)
+
+    master_action_spec = array_spec.BoundedArraySpec(
+        shape=((num_options,)), dtype=np.float32,
+        minimum=0, maximum=1, name='master_action'
+    )
+
+    options_observation_spec = array_spec.BoundedArraySpec(
+        shape=((num_options + num_state_bits), ), dtype=np.float32,
+        minimum=0, maximum=1, name='option_observation'
+    )
+    options_action_spec = array_spec.BoundedArraySpec(
+        shape=(num_state_bits, 2), dtype=np.float32,
+        minimum=0, maximum=1, name='option_action'
+    )
+
+    options_time_step_spec = ts.TimeStep(
+        step_type=train_env.time_step_spec().step_type,
+        reward=train_env.time_step_spec().reward,
+        discount=train_env.time_step_spec().discount,
+        observation=options_observation_spec
+    )
+
+    num_actions = train_env.action_spec().maximum - train_env.action_spec().minimum + 1
+    low_level_model, callbacks = setup_model(
+        num_actions, num_state_bits, sae, low_level_model_path)
+
+    low_level_env = LowLevelEnv(train_env, low_level_model)
+
+    options_env = OptionsEnv(
+        low_level_env, options_observation_spec, options_action_spec)
+    option_train_env = tf_py_environment.TFPyEnvironment(options_env)
+
+    master_env = MasterEnv(
+        low_level_env, master_action_spec)
+    master_train_env = tf_py_environment.TFPyEnvironment(master_env)
+
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+    global_step = tf.compat.v1.train.get_or_create_global_step()
+
+    master_value_network = value_network.ValueNetwork(
+        master_train_env.time_step_spec().observation,
+        fc_layer_params=(100,)
+    )
+
+    master_actor_network = actor_distribution_network.ActorDistributionNetwork(
+        master_train_env.time_step_spec().observation,
+        master_train_env.action_spec(),
+        fc_layer_params=(100,)
+    )
+
+    master_agent = ppo_agent.PPOAgent(
+        master_train_env.time_step_spec(),
+        master_train_env.action_spec(),
+        optimizer=optimizer,
+        actor_net=master_actor_network,
+        value_net=master_value_network,
+        train_step_counter=tf.Variable(0)
+    )
+    master_agent.initialize()
+    master_agent.train = common.function(master_agent.train)
+    options_env.set_master_policy(master_agent.policy)
+
+    options_actor_network = OptionsNetwork(
+        option_train_env.time_step_spec().observation,
+        option_train_env.action_spec(),
+        num_options
+    )
+
+    options_value_network = value_network.ValueNetwork(
+        option_train_env.time_step_spec().observation,
+        fc_layer_params=(100,)
+    )
+
+    options_agent = ppo_agent.PPOAgent(
+        option_train_env.time_step_spec(),
+        option_train_env.action_spec(),
+        optimizer=optimizer,
+        actor_net=options_actor_network,
+        value_net=options_value_network,
+        train_step_counter=tf.Variable(0)
+    )
+    options_agent.initialize()
+    options_agent.train = common.function(options_agent.train)
+    master_env.set_options_policy(options_agent.policy)
+
+
+    master_rb = create_replay_buffer(
+        master_agent, batch_size, replay_buffer_max_length)
+    options_rb = create_replay_buffer(
+        options_agent, batch_size, replay_buffer_max_length)
+
+    master_ds = master_rb.as_dataset(
+        num_parallel_calls=3,
+        sample_batch_size=batch_size,
+        num_steps=2
+    )
+    master_iter = iter(master_ds)
+    options_ds = options_rb.as_dataset(
+        num_parallel_calls=3,
+        sample_batch_size=batch_size,
+        num_steps=2
+    )
+    options_iter = iter(options_ds)
+
+    master_checkpointer = create_train_checkpointer(
+        checkpoint_dir + "master/", master_agent, master_rb, global_step)
+    options_checkpointer = create_train_checkpointer(
+        checkpoint_dir + "options/", options_agent, options_rb, global_step)
+
+    master_saver = policy_saver.PolicySaver(master_agent.policy)
+    options_saver = policy_saver.PolicySaver(options_agent.policy)
+
+
+    def check_interval(interval):
+        return global_step % interval == 0
+
+
+    while (global_step < num_iterations):
         populate_buffer(
             master_train_env, master_rb,
             master_agent.collect_policy,
             master_agent.time_step_spec,
-            2,
+            master_collect_steps,
             batch_size
         )
-        populate_buffer(
-            option_train_env, options_rb,
-            options_agent.collect_policy,
-            options_agent.time_step_spec,
-            2,
-            batch_size
-        )
-        option_exp, unused_info = next(options_iter)
-        options_loss = options_agent.train(option_exp)
-        master_exp, unused_info = next(master_iter)
-        master_loss = master_agent.train(master_exp)
+        for _ in range(warmup_period):
+            experience, unused_info = next(master_iter)
+            master_loss = master_agent.train(experience)
 
-    if check_interval(log_interval):
-        print(
-            'step = {0}: master loss = {1}, options loss = {2}'.format(
-                global_step, master_loss, options_loss)
-        )
+        for _ in range(joint_update_period):
+            populate_buffer(
+                master_train_env, master_rb,
+                master_agent.collect_policy,
+                master_agent.time_step_spec,
+                2,
+                batch_size
+            )
+            populate_buffer(
+                option_train_env, options_rb,
+                options_agent.collect_policy,
+                options_agent.time_step_spec,
+                2,
+                batch_size
+            )
+            option_exp, unused_info = next(options_iter)
+            options_loss = options_agent.train(option_exp)
+            master_exp, unused_info = next(master_iter)
+            master_loss = master_agent.train(master_exp)
 
-    if check_interval(checkpoint_interval):
-        master_checkpointer.save(global_step)
-        options_checkpointer.save(global_step)
-        print('Checkpoint saved!')
+        if check_interval(log_interval):
+            print(
+                'step = {0}: master loss = {1}, options loss = {2}'.format(
+                    global_step, master_loss, options_loss)
+            )
 
-    # Reset master here
+        if check_interval(checkpoint_interval):
+            master_checkpointer.save(global_step)
+            options_checkpointer.save(global_step)
+            print('Checkpoint saved!')
 
-master_saver.save(save_dir + "master/")
-options_saver.save(save_dir + "options/")
-print("Policies Saved!")
+        # Reset master here
+
+    master_saver.save(save_dir + "master/")
+    options_saver.save(save_dir + "options/")
+    print("Policies Saved!")
+
+
+if __name__ == "__main__":
+    train()
